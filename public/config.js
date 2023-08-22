e0f2616259b5a22eb0e7a850182dc0c955ab6dc7
fishnu223 <131819906+fishnu223@users.noreply.github.com>
2023-08-21 21:57:18 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://movies.mahadasu-vishnu.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "25abb782c549ccaeb424681a0dda0165"
 };
